5027b3f210f10865ff389372fde186410e3b4b1c
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-02-25 13:18:16 -0500

    Add files via upload

---
+++ src/Person.java
@@ -0,0 +1,22 @@
+public class Person {
+
+    private String name;
+    private String birthday;
+
+    public Person (String name, String birthday)
+    {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String getBirthday(){
+        return birthday;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    //Create a equals method here
+    
+}
---
+++ src/PersonTester.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class PersonTester
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+    }
+}
